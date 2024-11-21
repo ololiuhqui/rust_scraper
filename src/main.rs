d138ead4aa740861ef2be2214b5b6d2da26e1d0f
Ololiuhqui <ludovicocaluori@protonmail.com>
2024-11-21 12:50:33 +0100

    now it can get all events

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{ fmt::format, ops::Index, option };
 
 use reqwest::{ self, blocking::get, Error, Version };
 use scraper::{
-    element_ref::Select,
+    element_ref::{ self, Select },
     error,
     html,
     node::Element,
@@ -37,7 +37,7 @@ impl Event {
     fn event_constructor(base_url: &str, node: ElementRef<'_>) -> Self {
         /* Finding rel_permalink from cards */
         let optional_rel_permalink = match node.value().attr("href") {
-            None => "".to_string(),
+            None => panic!("Could not get \"href\" attribute from card"),
             Some(href) => href.to_string(),
         };
 
@@ -53,50 +53,54 @@ impl Event {
         /* Getting title */
         let title_sel = match Selector::parse("h1") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(err) => panic!("Couldnt parse title selector: {err:?}"),
         };
         let title_node = event_page.select(&title_sel);
 
         /* Getting subtitle */
         let subtitle_sel = match Selector::parse("h1+ h4 ") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(err) => panic!("Couldnt parse subtitle selector: {err:?}"),
         };
         let subtitle_node = event_page.select(&subtitle_sel);
 
         /* Getting description title */
         let description_title_sel = match Selector::parse("#_event_estesa h4") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(err) => panic!("Couldnt parse description title selector: {err:?}"),
         };
         let description_title_nodes = event_page.select(&description_title_sel);
 
         /* Getting description */
         let description_sel = match Selector::parse("#_event_estesa  p") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(err) => panic!("Couldnt parse description selector: {err:?}"),
         };
         let description_nodes = event_page.select(&description_sel);
 
         /* Getting location */
         let location_sel = match Selector::parse("#_event_luogo h5") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(err) => panic!("Couldnt parse location selector: {err:?}"),
         };
         let location_nodes = event_page.select(&location_sel);
 
         /* Getting post image */
+
         let image_sel = match Selector::parse(".col-lg-8 img") {
             Ok(selector) => selector,
-            Err(err) => panic!("Couldnt parse selector: {err:?}"),
+            Err(first_selector_err) => {
+                println!(
+                    "First selector couldn't select an image ({first_selector_err:?}), trying another one..."
+                );
+                match Selector::parse(".content-image") {
+                    Ok(selector_alt) => selector_alt,
+                    Err(err) => panic!("Couldn't get image from second selector: {err:?}"),
+                }
+            }
         };
-        let image_node = event_page.select(&image_sel);
-        let image_rel_permalink = get_single_elementref(image_node)
-            .expect("couldn't get image")
-            .value()
-            .attr("src")
-            .expect("couldn't get image source")
-            .to_string();
+        let image_node: html::Select<'_, '_> = event_page.select(&image_sel);
+        let image_rel_permalink = get_element_attrib_src(image_node);
 
         /* Building struct */
         Self {
@@ -127,14 +131,6 @@ fn get_document(permalink: &str) -> Result<Html, reqwest::Error> {
     Ok(document)
 }
 
-fn get_single_elementref<'a>(nodes: scraper::html::Select<'a, '_>) -> Option<ElementRef<'a>> {
-    for node in nodes {
-        return Some(node);
-    }
-
-    None
-}
-
 /* This function gets element values from selected nodes*/
 fn get_element_value(nodes: scraper::html::Select<'_, '_>) -> String {
     let mut joined_text: String = "".to_string();
@@ -153,6 +149,20 @@ fn get_element_value(nodes: scraper::html::Select<'_, '_>) -> String {
     joined_text
 }
 
+fn get_element_attrib_src(nodes: scraper::html::Select<'_, '_>) -> String {
+    let mut src = "".to_string();
+    for node in nodes {
+        let src_value = node
+            .value()
+            .attr("src")
+            .expect("couldn't get image source HTML attribute")
+            .to_string();
+        src.push_str(&src_value);
+    }
+
+    src
+}
+
 /**
  * Builds a permalink from a base url and a relative permalink
  */
@@ -160,14 +170,6 @@ fn build_permalink(base_url: &str, rel_permalink: &str) -> String {
     let permalink = format!("{}{}", base_url, rel_permalink);
     permalink
 }
-
-//#TODO
-/* fn check_if_date(date: Option<String>) -> Option<String> {
-    match date {
-        Some(date) => Some(date),
-        None => None,
-    }
-} */
 
 /**
  * main
@@ -179,15 +181,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let card_link_sel = Selector::parse(".eventi-elenco .cmp-list-card-img__body-title a")?;
     let card_link_nodes: html::Select<'_, '_> = mydocument.select(&card_link_sel);
 
-    let optional_card_link_node = get_single_elementref(card_link_nodes);
-    let card_link_node = match optional_card_link_node {
-        Some(card_link_node) => card_link_node,
-        None => panic!("sad"),
-    };
+    let mut events: Vec<Event> = vec![];
 
-    let event1 = Event::event_constructor(SITO_COMUNE, card_link_node);
+    for node in card_link_nodes {
+        let event = Event::event_constructor(SITO_COMUNE, node);
 
-    dbg!(event1);
+        events.push(event);
+        // come si vede dall'output solo il primo evento viene correttamente tirato fuori dal Select
+    }
+
+    dbg!(&events);
 
     Ok(())
 }
